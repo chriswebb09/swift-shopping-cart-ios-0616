a4a4e7d01fb8c0cbbec70f8f82ad406e0d215e98
chriswebb09 <chris.webb5249@gmail.com>
2016-07-24 15:52:53 -0400

    Done

--- Cart.swift
+++ Cart.swift
@@ -23,25 +23,54 @@ class Cart {
         return total
     }
     
-    func addItem(_:Item) {
-        self.items.addItem(_)
+    func addItem(item:Item) {
+        self.items.append(item)
     }
     
     
-    func removeItem(_:Item) {
-        sel
+    func removeItem(item: Item) {
+        if let itemIndex = self.items.indexOf(item) {
+            self.items.removeAtIndex(itemIndex)
+        }
     }
     
-    func itemsWithName(_:String) -> [Item] {
+    func itemsWithName(name:String) -> [Item] {
+        var returnArray: [Item] = []
+        for item in self.items {
+            if item.name == name {
+                returnArray.append(item)
+            }
+        }
+        return returnArray
+    }
+    
+    func totalPriceInCents() -> Int {
+        var total = 0
+        for item in self.items {
+            total += item.priceInCents
+        }
+        return total
+    }
+    
+    func itemsWithMinimumPriceInCents(price:Int) -> [Item] {
+        var itemArray: [Item] = []
+        for item in self.items {
+            if item.priceInCents >= price {
+                itemArray.append(item)
+            }
+        }
+        return itemArray
         
     }
     
-    func itemsWithMinimumPriceInCents(_:Int) -> [Item] {
-        
-    }
-    
-    func itemsWithMaximumPriceInCents(_:Int) -> [Item] {
-        
+    func itemsWithMaximumPriceInCents(price:Int) -> [Item] {
+        var itemArray: [Item] = []
+        for item in self.items {
+            if item.priceInCents <= price {
+                itemArray.append(item)
+            }
+        }
+        return itemArray
     }
 }
 
